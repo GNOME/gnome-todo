82ef8e35a46957d01e75278bcfe7e774319ab461
Shashwat <shashwat.jolly@gmail.com>
2017-12-30 18:15:45 +0530

    Added new variable for copyright year

--- src/gtd-application.c
+++ src/gtd-application.c
@@ -117,6 +117,7 @@ gtd_application_show_about (GSimpleAction *simple,
   g_autoptr (GDateTime) date = NULL;
   GtdApplication *self;
   gint created_year = 2015;
+  gint modified_year = 2017;
 
   static const gchar *authors[] = {
     "Emmanuele Bassi <ebassi@gnome.org>",
@@ -145,7 +146,7 @@ gtd_application_show_about (GSimpleAction *simple,
   else
     {
       copyright = g_strdup_printf (_("Copyright \xC2\xA9 %1$d\xE2\x80\x93%2$d "
-                                     "The To Do authors"), created_year, g_date_time_get_year (date));
+                                     "The To Do authors"), created_year, modified_year);
     }
 
   gtk_show_about_dialog (GTK_WINDOW (self->window),

8340bfb14e92e22bada54ccc533f2f645153aff9
AndreiLisita <andreii.lisita@gmail.com>
2018-02-11 20:27:54 +0000

    Revert "task-list-view: Done button has maximum width"
    This reverts commit 553d14446afbe0eb23f5d90108004fee9e6ed73c

--- src/gtd-task-list-view.c
+++ src/gtd-task-list-view.c
@@ -29,7 +29,6 @@
 #include "gtd-task.h"
 #include "gtd-task-list.h"
 #include "gtd-task-row.h"
-#include "gtd-done-button.h"
 #include "gtd-window.h"
 
 #include <glib.h>
@@ -1524,7 +1523,6 @@ gtd_task_list_view_class_init (GtdTaskListViewClass *klass)
   g_type_ensure (GTD_TYPE_TASK_ROW);
   g_type_ensure (GTD_TYPE_DND_ROW);
   g_type_ensure (GTD_TYPE_EMPTY_LIST_WIDGET);
-  g_type_ensure (GTD_TYPE_DONE_BUTTON);
 
   /**
    * GtdTaskListView::color:

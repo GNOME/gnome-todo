fb42bfd81e62b6e1f8b89b5f65121bdf97404f2b
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2018-05-24 15:50:30 -0300

    ci: Add development flatpak bundle generation

---
+++ build-aux/ci/build-flatpak.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+PROJECT_ID=org.gnome.$1
+BUNDLE=$PROJECT_ID.flatpak
+MANIFEST=$PROJECT_ID.json
+RUNTIME_REPO="https://sdk.gnome.org/gnome-nightly.flatpakrepo"
+
+flatpak-builder --stop-at=gnome-todo --bundle-sources --repo=repo app $MANIFEST
+
+flatpak build app meson . _build --prefix=/app -Dtracing=true
+flatpak build app ninja -C _build install
+flatpak-builder --finish-only --repo=repo app $MANIFEST
+
+flatpak build-bundle repo $BUNDLE --runtime-repo=$RUNTIME_REPO $PROJECT_ID 
